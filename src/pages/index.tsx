97a432d300178c2f66d971a78cf1d72cdf9b3103
JithinKS97 <jithinks112@gmail.com>
2023-04-03 13:36:03 +0530

    removed app folder and added pages folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Box p="10">
+        <Heading>
+            Problem2Profit
+        </Heading>
+        <Box mt= "5">
+        Problem2Profit is a digital platform designed to inspire Indian entrepreneurs by presenting a curated list of real-world issues that can be transformed into viable business ventures. By targeting India-specific challenges, the platform encourages innovation and empowers the youth to generate wealth while creating a positive impact on society. The platform showcases expert-endorsed opportunities, ranks them based on relevance, and offers resources for users to develop effective solutions. Revenue for the platform will be generated through advertisements from services catering to startups and small businesses. Problem2Profit aims to be a vital resource for entrepreneurs, specialists, investors, and organizations dedicated to addressing India's most pressing problems through inventive and sustainable approaches.
+        </Box>
+    </Box>
+  )
+}
