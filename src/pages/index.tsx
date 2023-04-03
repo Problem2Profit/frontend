3eb3d6ba321955b864e60964eafacd1395036a12
JithinKS97 <jithinks112@gmail.com>
2023-04-03 14:26:25 +0530

    reorganised

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, ListItem, UnorderedList } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -7,7 +7,23 @@ export default function Home() {
             Problem2Profit
         </Heading>
         <Box mt= "5">
-          {`Problem2Profit is a digital platform designed to inspire Indian entrepreneurs by presenting a curated list of real-world issues that can be transformed into viable business ventures. By targeting India-specific challenges, the platform encourages innovation and empowers the youth to generate wealth while creating a positive impact on society. The platform showcases expert-endorsed opportunities, ranks them based on relevance, and offers resources for users to develop effective solutions. Revenue for the platform will be generated through advertisements from services catering to startups and small businesses. Problem2Profit aims to be a vital resource for entrepreneurs, specialists, investors, and organizations dedicated to addressing India's most pressing problems through inventive and sustainable approaches.`}
+          <UnorderedList>
+            <ListItem mt="2">
+              {`Problem2Profit is a digital platform designed to inspire Indian entrepreneurs by presenting a curated list of real-world issues that can be transformed into viable business ventures.`}
+            </ListItem>
+            <ListItem mt="2">
+              {`By targeting India-specific challenges, the platform encourages innovation and empowers the youth to generate wealth while creating a positive impact on society.`}
+            </ListItem>
+            <ListItem mt="2">
+              {`The platform showcases expert-endorsed opportunities, ranks them based on relevance, and offers resources for users to develop effective solutions.`}
+            </ListItem>
+            <ListItem mt="2">
+              {`Revenue for the platform will be generated through advertisements from services catering to startups and small businesses.`}
+            </ListItem>
+            <ListItem mt="2">
+              {`The platform will also offer a subscription-based service for users to access premium content and resources.`}
+            </ListItem>
+          </UnorderedList>
         </Box>
     </Box>
   )
